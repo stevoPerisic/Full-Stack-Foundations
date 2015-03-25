75463778d82c5062f64541eb911c986278ac7343
stevoperisic <stevoperisic@MacBook-Pro.local>
2015-03-24 23:04:48 -0400

    added DB creation, fixed bug
    * added DB creation to all the sub-projects
    * fixed spelling bug in Objective 4

--- Lesson-2/Objective-4-Solution/webserver.py
+++ Lesson-2/Objective-4-Solution/webserver.py
@@ -82,8 +82,12 @@ class webServerHandler(BaseHTTPRequestHandler):
             if self.path.endswith("/edit"):
                 ctype, pdict = cgi.parse_header(
                     self.headers.getheader('content-type'))
-                if ctype == 'mulitpart/form-data':
+
+                print ctype
+
+                if ctype == 'multipart/form-data':
                     fields = cgi.parse_multipart(self.rfile, pdict)
+
                     messagecontent = fields.get('newRestaurantName')
                     restaurantIDPath = self.path.split("/")[2]
 
